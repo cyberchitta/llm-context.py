391974412f2b70550f91b86824c0259bbbe16057
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-04-25 13:50:34 +0530

    refactor

--- src/config_manager.py
+++ src/config_manager.py
@@ -10,21 +10,21 @@ class ConfigManager:
     def create_default():
         default_user = {
             "root_path": os.getcwd(),
-            "templates_path": os.path.expanduser("~/Github/llm-context/templates"),
+            "templates_path": os.path.expanduser("~/.llm-context/templates"),
         }
         default_project = {"template": "all-file-contents.j2", "files": [], "gitignores": [".git"]}
         user_file = ConfigManager.user_file
+        ConfigManager.ensure_exists(user_file, default_user)
         project_file = ConfigManager.get_project_file(user_file)
+        ConfigManager.ensure_exists(project_file, default_project)
 
-        return ConfigManager.create(user_file, project_file, default_user, default_project)
+        return ConfigManager.create(user_file, project_file)
 
     @staticmethod
-    def create(user_file, project_file, default_user, default_project):
-        config_manager = ConfigManager(project_file)
-        config_manager.ensure_exists(user_file, default_user)
-        config_manager.ensure_exists(project_file, default_project)
-        config_manager.load(user_file, project_file)
-        return config_manager
+    def create(user_file, project_file):
+        user = ConfigManager._load(user_file)
+        project = ConfigManager._load(project_file)
+        return ConfigManager(project_file, user, project)
 
     @staticmethod
     def get_project_file(user_file):
@@ -37,31 +37,30 @@ class ConfigManager:
         with open(file, "r") as f:
             return json.load(f)
 
-    def __init__(self, project_file):
-        self.project_file = project_file
-        self.user = None
-        self.project = None
-
-    def ensure_exists(self, file, default):
+    @staticmethod
+    def ensure_exists(file, default):
         if not os.path.exists(file):
-            self.save(file, default)
+            ConfigManager.save(file, default)
 
-    def load(self, user_file, project_file):
-        self.user = self._load(user_file)
-        self.project = self._load(project_file)
-
-    def save(self, file, config):
+    @staticmethod
+    def save(file, config):
         directory = os.path.dirname(file)
         os.makedirs(directory, exist_ok=True)
         with open(file, "w") as f:
             json.dump(config, f, indent=2)
+
+    def __init__(self, project_file, user, project):
+        self.project_file = project_file
+        self.user = user
+        self.project = project
+
 
     def templates_path(self):
         return self.user["templates_path"]
 
     def update_files(self, files):
         self.project["files"] = files
-        self.save(self.project_file, self.project)
+        ConfigManager.save(self.project_file, self.project)
 
 
 def main():
